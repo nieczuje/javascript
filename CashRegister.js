73cf1762f4e695ab518a5e96de9234879130bc79
Tomasz <tomeq13@interia.pl>
2019-12-31 21:53:34 +0100

    Update CashRegister.js
    testttt

--- CashRegister.js
+++ CashRegister.js
@@ -1,4 +1,6 @@
 
+// JavaScript Algorithms and Data Structures Projects: Cash Register
+// https://www.freecodecamp.org/learn/javascript-algorithms-and-data-structures/javascript-algorithms-and-data-structures-projects/cash-register
 
 function checkCashRegister(price, cash, cid) {
   var result;
